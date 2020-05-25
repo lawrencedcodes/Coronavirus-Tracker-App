345be408e06abaa73b6d6fd722e8eeb31e793910
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-05-25 00:49:11 -0500

    Loop through all records in the csv

--- src/main/java/com/topsoutherncoders/rona/RonaApplication.java
+++ src/main/java/com/topsoutherncoders/rona/RonaApplication.java
@@ -2,8 +2,10 @@ package com.topsoutherncoders.rona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RonaApplication {
 
 	public static void main(String[] args) {
