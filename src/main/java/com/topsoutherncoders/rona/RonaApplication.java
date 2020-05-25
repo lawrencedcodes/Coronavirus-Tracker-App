f1aee72c080699387bc478d8ad492897461ac501
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-05-24 19:09:17 -0500

    spring init

---
+++ src/main/java/com/topsoutherncoders/rona/RonaApplication.java
@@ -0,0 +1,13 @@
+package com.topsoutherncoders.rona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topsoutherncoders/rona/RonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topsoutherncoders.rona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
